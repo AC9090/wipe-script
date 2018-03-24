39fc2074cefef4a485f4f04f45ee3ad3f6adc152
andrew@tt <andrew@tt>
2018-03-24 14:38:09 +0000

    fixed query

--- sql_copy.c
+++ sql_copy.c
@@ -12,26 +12,16 @@ void finish_with_error(MYSQL *con)
   exit(1);        
 }
 
-void print_to_csv(FILE * f, MYSQL mysql, char * table)
+void print_to_csv(FILE * f, MYSQL mysql, char * table, MYSQL_RES *result)
 {
 
-	char * query = malloc(sizeof(char) * 256);
 
-	sprintf(query,"SELECT * FROM disk d1 WHERE wiped = (SELECT max(wiped) FROM disk d3 WHERE d3.disk_serial = d1.disk_serial) and synced = 0;", table);
-	
-	if (mysql_query(&mysql, query))
-    finish_with_error(&mysql);
-  
-	MYSQL_RES *result = mysql_store_result(&mysql);
-			  
-	if (result == NULL)
-    	finish_with_error(&mysql);
 
 	unsigned int res_count = mysql_num_rows(result);
 	unsigned int num_fields = mysql_num_fields(result);
 	unsigned int i;
 	MYSQL_ROW row;
-
+	
 	if (res_count == 0) {
 		printf("No unsynced records were found for table %s\n", table);
 		return;
@@ -56,6 +46,7 @@ void print_to_csv(FILE * f, MYSQL mysql, char * table)
 	  	fprintf(f, "\n");
 	  	printf("\n");
 	}
+	char * query = malloc(sizeof(char) * 512);
 
 	printf("Saved %d rows from %s to file.\n", res_count, table);
 	sprintf(query,"UPDATE %s SET  sync_time=CURRENT_TIMESTAMP, synced=1 WHERE synced=0;", table);
@@ -111,8 +102,32 @@ int main(int argc, char *argv[])
 	}
 	printf("Using file %s \n", filename);
 
-	print_to_csv(f, mysql, "disk");
-	print_to_csv(f, mysql, "computer");
+
+	char * query = malloc(sizeof(char) * 512);
+
+	sprintf(query,"SELECT * FROM disk d1 WHERE wiped = (SELECT max(wiped) FROM disk d3 WHERE d3.disk_serial = d1.disk_serial) and synced = 0;");
+	
+	if (mysql_query(&mysql, query))
+    	finish_with_error(&mysql);
+  
+	MYSQL_RES *result = mysql_store_result(&mysql);
+			  
+	if (result == NULL)
+    	finish_with_error(&mysql);
+
+	print_to_csv(f, mysql, "disk", result);
+
+	sprintf(query, "SELECT * FROM computer WHERE synced = 0");
+
+	if (mysql_query(&mysql, query))
+    	finish_with_error(&mysql);
+  
+	result = mysql_store_result(&mysql);
+			  
+	if (result == NULL)
+    	finish_with_error(&mysql);
+
+	print_to_csv(f, mysql, "computer", result);
 	fclose(f);
 
 
