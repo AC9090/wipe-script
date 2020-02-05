9cdbc2cf25ad102a19477e84ac35501f3be48bac
andrew@tt <andrew@tt>
2020-02-05 13:20:40 +0000

    Add Restart

---
+++ wiper/Restart.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Restart Networking"
+echo " Q to continue"
+sudo service isc-dhcp-server restart
+sudo service tftpd-hpa restart
+sudo service smbd restart
+sudo service nmbd restart
+sudo service isc-dhcp-server status
+sudo service tftpd-hpa status
+sudo service smbd status
+sudo service nmbd restart
+sudo service nmbd status
